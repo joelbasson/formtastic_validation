5b9cb1975db394104e9342e94baa1957bb83dc14
joelbasson <joelbasson@me.com>
2010-11-03 01:10:35 +0200

    refactor

--- lib/generators/formtastic_validation_generator.rb
+++ lib/generators/formtastic_validation_generator.rb
@@ -1,17 +1,11 @@
 require 'rails/generators'
 
 class FormtasticValidationGenerator < Rails::Generators::Base
-  def manifest
-    record do |m|
+  
+  def install_formtastic_validations
       copy_file 'templates/public/javascripts/formtastic_validation-0.1.2.min.js', 'public/javascripts/formtastic_validation-0.1.2.min.js'
       copy_file 'templates/public/javascripts/livevalidation-1.3.min.js', 'public/javascripts/livevalidation-1.3.min.js'
       copy_file 'templates/config/initializers/formtastic_validation.rb', 'config/initializers/formtastic_validation.rb'
-    end
   end
-
-  protected
-
-  def banner
-    %\Usage: #{$0} formtastic_validation\
-  end
+  
 end
